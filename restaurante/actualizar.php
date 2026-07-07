d2cb5e6c3da237e8a41f8d679dc80d81e4b19a30
majo876 <riosmajo90@gmail.com>
2026-07-07 07:33:21 -0500

    proyecto de grado

--- restaurante/actualizar.php
+++ restaurante/actualizar.php
@@ -24,6 +24,7 @@ if(!$usuario) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/estilos.css">
 </head>
 <body>
     <form action="procesar_actualizacion.php" method="POST">
--- restaurante/lista.php
+++ restaurante/lista.php
@@ -12,9 +12,10 @@ $usuario = $result->fetchALL();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     <link rel="stylesheet" href="../css/estilos.css">
 </head>
 <body>
-    <h1>Uusarios</h1>
+    <h1>Usuarios</h1>
     <a href="crear.php"><button>Crear Usuario</button></a>
     <table border=1>
         <tr>
